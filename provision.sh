0a05d0c428e7ca459ae9c1473622cb542c929a75
CruzanCaramele <letters2hamza@gmail.com>
2015-12-01 15:52:26 +0800

    create postgres database

--- provision.sh
+++ provision.sh
@@ -8,4 +8,10 @@ apt-get -qqy install python-pip
 apt-get -qqy install oauth2client
 apt-get -qqy install itsdangerous
 apt-get -qqy python-flask python-sqlalchemy
+apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy flask-httpauth
+
+su postgres -c 'createuser -dRS culture'
+su culture -c 'createdb'
+su culture -c 'createdb cultures'
+
