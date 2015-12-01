3d2bb4e1344080c8e610e315b69439e4c17bd0bf
CruzanCaramele <letters2hamza@gmail.com>
2015-12-01 15:48:37 +0800

    perform system update and install requirements

---
+++ provision.sh
@@ -0,0 +1,11 @@
+apt-get -qqy update
+
+apt-get -qqy install bleach
+apt-get -qqy install passlib
+apt-get -qqy install httplib2
+apt-get -qqy install requests
+apt-get -qqy install python-pip
+apt-get -qqy install oauth2client
+apt-get -qqy install itsdangerous
+apt-get -qqy python-flask python-sqlalchemy
+apt-get -qqy flask-httpauth
