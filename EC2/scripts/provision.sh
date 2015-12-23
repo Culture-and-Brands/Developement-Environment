03613a61d0698e04208ce28ba955554ed5c0e0ac
CruzanCaramele <letters2hamza@gmail.com>
2015-12-23 15:11:33 +0800

    add script file

---
+++ EC2/scripts/provision.sh
@@ -0,0 +1,30 @@
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install libffi-dev
+apt-get -qqy install python-dev
+apt-get -qqy install python-pip
+
+
+pip install Flask-SQLAlchemy
+pip install sqlalchemy-utils
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install passlib
+pip install itsdangerous
+pip install flask-httpauth
+pip install flask-bcrypt
+pip install Werkzeug
+pip install Flask-WTF
+pip install WTForms
+pip install arrow
+pip install flask-login
+
+
+
+
+su postgres -c 'createuser -dRS culture'
+su culture -c 'createdb'
+su culture -c 'createdb cultures'
