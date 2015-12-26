a7148816574bd56ab2f15d1a220d44a09efbe8c2
CruzanCaramele <letters2hamza@gmail.com>
2015-12-26 12:10:41 +0800

    include issing comma

--- EC2/puppet/modules/localusers/manifests/hamzafile.pp
+++ EC2/puppet/modules/localusers/manifests/hamzafile.pp
@@ -5,6 +5,6 @@ class localusers::hamzafile {
 	# resources
 	file { '/etc/sudoers.d/hamza':
 		ensure  => file,
-		content => 'hamza ALL=(ALL) NOPASSWD:ALL'
+		content => 'hamza ALL=(ALL) NOPASSWD:ALL',
 	}
 }
