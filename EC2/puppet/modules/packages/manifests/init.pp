760957ef0fcf563c779838fb18c493590be80fc9
CruzanCaramele <letters2hamza@gmail.com>
2015-12-26 12:11:40 +0800

    use puppet to install most packages

---
+++ EC2/puppet/modules/packages/manifests/init.pp
@@ -0,0 +1,45 @@
+# Class: packages
+#
+#
+
+class packages {
+
+	$installations = [
+		"python-setuptools",
+		"libapache2-mod-wsgi",
+		"python-flask",
+		"python-sqlalchemy",
+		"python-dev",
+		"python-pip",
+		"postgresql",
+		"python-psycopg2",
+		"libffi-dev"
+	]
+
+	$pipinstalls = [
+		"Flask-SQLAlchemy",
+		"sqlalchemy-utils",
+		"bleach",
+		"oauth2client",
+		"requests",
+		"httplib2",
+		"passlib",
+		"itsdangerous",
+		"flask-httpauth",
+		"flask-bcrypt",
+		"Werkzeug",
+		"Flask-WTF",
+		"WTForms",
+		"arrow",
+		"flask-login"
+	]
+	# resources
+	package { '$installations':
+		ensure => installed,
+	}
+
+	package { '$pipinstalls':
+		ensure   => installed,
+		provider => pip,
+	}
+}
