b94b1029e688cc856b711313984889fc73928841
CruzanCaramele <letters2hamza@gmail.com>
2015-12-26 12:29:30 +0800

    reorder update option

--- EC2/scripts/provision.sh
+++ EC2/scripts/provision.sh
@@ -1,28 +1,8 @@
 #!/bin/bash -eux
 
-sudo apt-get -y update
-sudo apt-get -y install python-flask python-sqlalchemy
-sudo apt-get -y install libffi-dev
-sudo apt-get -y install python-dev
-sudo apt-get -y install python-pip
-sudo apt-get -y install postgresql python-psycopg2
 sudo wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
+sudo apt-get -y update
 sudo apt-get -y install puppet
 
 
-sudo pip install Flask-SQLAlchemy
-sudo pip install sqlalchemy-utils
-sudo pip install bleach
-sudo pip install oauth2client
-sudo pip install requests
-sudo pip install httplib2
-sudo pip install passlib
-sudo pip install itsdangerous
-sudo pip install flask-httpauth
-sudo pip install flask-bcrypt
-sudo pip install Werkzeug
-sudo pip install Flask-WTF
-sudo pip install WTForms
-sudo pip install arrow
-sudo pip install flask-login
