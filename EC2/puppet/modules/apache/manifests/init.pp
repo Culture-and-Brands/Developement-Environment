9027a03cb8f1193db2ddd06eb43a3780d6dd017a
CruzanCaramele <letters2hamza@gmail.com>
2015-12-29 19:04:39 +0800

    remove unwanted vcsrepo

--- EC2/puppet/modules/apache/manifests/init.pp
+++ EC2/puppet/modules/apache/manifests/init.pp
@@ -7,6 +7,10 @@ class apache {
 		ensure => installed,
 	}
 
+	package { 'git':
+		ensure => installed,
+	}
+
 	service { 'apache2':
 		enable      => true,
 		ensure      => running,
@@ -15,12 +19,4 @@ class apache {
 		require     => Package["apache2"],
 	}
 
-	vcsrepo {'/var/www/GotRoom':
-		ensure   => latest,
-		provider => git,
-		source   => 'git://github.com/CruzanCaramele/GotRoom',
-		revision => 'PostgreSql',
-		require  => Package['apache2'],
-		notify   => Service['apache2'],
-	}
 }
