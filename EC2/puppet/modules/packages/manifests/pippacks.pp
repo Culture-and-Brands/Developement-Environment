5dfd4398ba67f8fb213f480fe446fcd4f2be1a1f
CruzanCaramele <letters2hamza@gmail.com>
2015-12-30 19:49:56 +0800

    install pip packages

---
+++ EC2/puppet/modules/packages/manifests/pippacks.pp
@@ -0,0 +1,31 @@
+# Class: packages::pippacks
+#
+#
+class packages::pippacks {
+	
+
+	$pipinstalls = [
+		"Flask-SQLAlchemy",
+		"sqlalchemy-utils",
+		"bleach",
+		"oauth2client",
+		"requests",
+		"httplib2",
+		"passlib",
+		"itsdangerous",
+		"flask-httpauth",
+		"Werkzeug",
+		"Flask-WTF",
+		"WTForms",
+		"arrow",
+		"flask-login",
+		"virtualenv",
+		"flask-bcrypt"
+	]
+
+	package { $pipinstalls:
+		ensure => installed,
+		require => [Package[$installs], Package[$installations]]
+	}
+
+}
