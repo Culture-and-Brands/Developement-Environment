89ef75c20184ac92b49afc57f159b59892b613bd
CruzanCaramele <letters2hamza@gmail.com>
2015-12-30 19:16:13 +0800

    include other classes in the module

--- EC2/puppet/modules/apache/manifests/init.pp
+++ EC2/puppet/modules/apache/manifests/init.pp
@@ -7,16 +7,7 @@ class apache {
 		ensure => installed,
 	}
 
-	package { 'git':
-		ensure => installed,
-	}
-
-	service { 'apache2':
-		enable      => true,
-		ensure      => running,
-		#hasrestart => true,
-		#hasstatus  => true,
-		require     => Package["apache2"],
-	}
+	include apache::service
+	include apache::apachedir
 
 }
