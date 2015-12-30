a7cb46c61e279e6a2fba152bb35c743ed49133e2
CruzanCaramele <letters2hamza@gmail.com>
2015-12-30 19:49:18 +0800

    include other classes in the module and seperate installations

--- EC2/puppet/modules/packages/manifests/init.pp
+++ EC2/puppet/modules/packages/manifests/init.pp
@@ -1,52 +1,14 @@
 # Class: packages
 #
-#
-
 class packages {
-
-	$installations = [
-		"python-setuptools",
-		"libapache2-mod-wsgi",
-		"python-flask",
-		"python-sqlalchemy",
-		"python-dev",
-		"postgresql",
-		"python-psycopg2",
-		"libffi-dev"
-	]
-
-	$pipinstalls = [
-		"Flask-SQLAlchemy",
-		"sqlalchemy-utils",
-		"bleach",
-		"oauth2client",
-		"requests",
-		"httplib2",
-		"passlib",
-		"itsdangerous",
-		"flask-httpauth",
-		"Werkzeug",
-		"Flask-WTF",
-		"WTForms",
-		"arrow",
-		"flask-login",
-		"virtualenv",
-		"flask-bcrypt"
-	]
 	# resources
-	package { 'python-pip':
+	$installs = ['python-pip', 'git']
+
+	package { $installs:
 		ensure => installed,
 	}
 
-	package { $installations:
-		ensure  => installed,
-		require => Package['python-pip'],
-	}
+	include packages::devpacks
+	include packages::pippacks
 
-
-	package { $pipinstalls:
-		ensure   => installed,
-		provider => 'pip',
-		require  => [Package['python-pip'], Package[$installations]],
-	}
 }
