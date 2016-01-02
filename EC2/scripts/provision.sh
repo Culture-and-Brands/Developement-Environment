794269ddfcf8e8dbff8c1dc496a91332fb2cb375
CruzanCaramele <letters2hamza@gmail.com>
2016-01-03 00:07:58 +0800

    install postgresql module before hand

--- EC2/scripts/provision.sh
+++ EC2/scripts/provision.sh
@@ -4,3 +4,4 @@ sudo wget http://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get -y update
 sudo apt-get -y install puppet
+sudo puppet module install puppetlabs-postgresql
