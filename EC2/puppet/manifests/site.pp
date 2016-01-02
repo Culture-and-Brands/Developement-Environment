3ff19dc6c81e4ca8473a895a7d1577b9467183f3
CruzanCaramele <letters2hamza@gmail.com>
2016-01-03 00:06:09 +0800

    manage db from here for now

--- EC2/puppet/manifests/site.pp
+++ EC2/puppet/manifests/site.pp
@@ -1,7 +1,24 @@
-node default {
-	include packages
-	include apache
-	include ssh
-	include localusers
-	include ntp
+ node default {
+  include packages
+  include apache
+  include ssh
+  include localusers
+  include ntp
+  
+}
+class { 'postgresql::globals':
+  manage_package_repo => true,
+  version             => '9.3',
+}->
+class { 'postgresql::server':
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  ipv4acls                   => ['hostssl all johndoe 192.168.0.0/24 cert'],
+  postgres_password          => 'TPSrep0rt!',
+}
+
+postgresql::server::db { 'zeeman':
+  user     => 'hamza',
+  password => postgresql_password('hamza', 'sauda'),
 }
