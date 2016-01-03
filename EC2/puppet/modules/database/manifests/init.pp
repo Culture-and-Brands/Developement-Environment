79f07425cab0b080cf1314b7105c7de97c6fe94c
CruzanCaramele <letters2hamza@gmail.com>
2016-01-03 20:12:11 +0800

    include db management

--- EC2/puppet/modules/database/manifests/init.pp
+++ EC2/puppet/modules/database/manifests/init.pp
@@ -1,10 +1,29 @@
-# # Class: database
-# #
-# #
-# class database {
-# 	# resources
-# 	package { ['postgresql', 'postgresql-contrib']:
-# 		ensure => installed,
-# 	}
+# Class: database
+#
+#
+class database {
+	# resources
+    class { 'postgresql::globals':
+        version             => '9.3',
+        manage_package_repo => true,
+        encoding            => 'UTF8',
+        locale              => 'en_US.UTF-8',
+    }->
+    class { 'postgresql::server':
+		ip_mask_allow_all_users    => '0.0.0.0/0',
+		listen_addresses           => '*',
+		postgres_password          => 'TPSrep0rt!',
+    }
 
-# }
+    # install the postgresql contrib package
+    class { 'postgresql::server::contrib':
+        package_ensure => 'present',
+    }
+
+    # create database with user and default permissions
+    postgresql::server::db { 'my_awesome_db':
+        user     => 'my_db_user',
+        password => 'puppetRocks',
+    }
+
+}
